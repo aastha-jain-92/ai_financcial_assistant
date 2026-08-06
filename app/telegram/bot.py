f8edc609ef5e8098812344d90af00a026580c2ea
aastha-jain-92 <aastha345jain@gmail.com>
2026-08-06 19:24:55 +0530

    Updated the bot.py logic of chat

--- app/telegram/bot.py
+++ app/telegram/bot.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from telegram.ext import MessageHandler, filters
 
 from telegram import Update
 from telegram.ext import (
@@ -12,6 +13,12 @@ load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+
+    await update.message.reply_text(
+        f"You said: {user_message}"
+    )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
@@ -22,6 +29,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 application = Application.builder().token(TOKEN).build()
 
 application.add_handler(CommandHandler("start", start))
+application.add_handler(MessageHandler(filters.TEXT & filters.COMMAND, echo))
 
 if __name__ == "__main__":
     application.run_polling()
